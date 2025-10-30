a634454196e1dc1084b049a67d3b4801ba099095
Dogukan-Ars <arss.dogukan@gmail.com>
2025-10-30 20:20:04 +0300

    update final form of hw

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
     define: {
       [isBuild ? '_global' : 'global']: {},
     },
-    base: isBuild ? '/goit-js-hw-11/' : '/',
+    base: isBuild ? '/goit-js-hw-12/' : '/',
     root: 'src',
     build: {
       sourcemap: true,
